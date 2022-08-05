ae9c0f8e5e54b9d3895f2659566b2eb9d92cebac
Simon vom Eyser <simon.vom.eyser@gmail.com>
2022-08-05 11:12:52 +0200

    feat: copy over svgs

---
+++ svg/index.js
@@ -0,0 +1,29 @@
+import logoNavigation from 'src/svg/logo-navigation.svg'
+import logo from 'src/svg/logo.svg'
+import user from 'src/svg/user.svg'
+import projects from 'src/svg/projects.svg'
+import write from 'src/svg/write.svg'
+import paperplane from 'src/svg/paper-plane.svg'
+import legal from 'src/svg/legal.svg'
+import unitedstates from 'src/svg/united-states.svg'
+import germany from 'src/svg/germany.svg'
+import xing from 'src/svg/xing.svg'
+import mail from 'src/svg/mail.svg'
+import twitter from 'src/svg/twitter.svg'
+import arrowDown from 'src/svg/arrow-down.svg'
+import externalLink from 'src/svg/external-link.svg'
+
+export const LogoNavigationSvg = logoNavigation
+export const LogoSvg = logo
+export const UserSvg = user
+export const ProjectsSvg = projects
+export const WriteSvg = write
+export const PaperPlaneSvg = paperplane
+export const LegalSvg = legal
+export const UnitedStatesSvg = unitedstates
+export const GermanySvg = germany
+export const TwitterSvg = twitter
+export const MailSvg = mail
+export const XingSvg = xing
+export const ArrowDown = arrowDown
+export const ExternalLink = externalLink
