a376374df14bb8af78a0c77688d0ae464621d159
Simon vom Eyser <simon.vom.eyser@gmail.com>
2022-08-05 10:34:08 +0200

    feat: create basic layout, copy over colors

--- next.config.js
+++ next.config.js
@@ -2,6 +2,19 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  i18n: {
+    locales: ['en', 'de'],
+    defaultLocale: 'en',
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"]
+    });
+
+    return config;
+  }
 }
 
 module.exports = nextConfig
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'white': 'var(--white)',
+        'black': 'var(--black)',
+        'neutral-100': 'var(--neutral-100)',
+        'neutral-200': 'var(--neutral-200)',
+        'neutral-250': 'var(--neutral-250)',
+        'neutral-300': 'var(--neutral-300)',
+        'neutral-400': 'var(--neutral-400)',
+        'neutral-500': 'var(--neutral-500)',
+        'neutral-600': 'var(--neutral-600)',
+        'neutral-700': 'var(--neutral-700)',
+        'teal-100': 'var(--teal-100)',
+        'teal-200': 'var(--teal-200)',
+        'teal-300': 'var(--teal-300)',
+        'teal-400': 'var(--teal-400)',
+        'teal-500': 'var(--teal-500)',
+        'teal-600': 'var(--teal-600)',
+        'red-100': 'var(--red-100)',
+        'red-200': 'var(--red-200)',
+        'red-300': 'var(--red-300)',
+        'red-400': 'var(--red-400)',
+        'primary': 'var(--primary)',
+      },
+    },
   },
   plugins: [],
 }
